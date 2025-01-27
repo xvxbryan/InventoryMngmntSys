64fde3886b3e66700e5a41d7c51c75750b7bdfe9
xvxbryan <bryanbastida17@yahoo.com>
2025-01-26 19:03:37 -0500

    adding front end client

---
+++ client/app/categories/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ViewCategories from '../components/ViewCategories'
+
+const Categories = () => {
+  return (
+    <div className="p-5">
+        <h1 className="mb-5 text-3xl font-semibold">Categories</h1>
+        <ViewCategories/>
+    </div>
+  )
+}
+
+export default Categories
